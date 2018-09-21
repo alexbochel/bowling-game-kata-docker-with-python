3d0b6746898a5a1b7d90541695bb43f5606381f7
Alexander Bochel <alexander.bochel@abochel-linux.2ldxhoqz4w0efi5ajh3u0k0m2g.bx.internal.cloudapp.net>
2018-09-21 16:14:40 +0000

    Finished unit test and implementation for bowling all zeroes

--- bowling.py
+++ bowling.py
@@ -1,3 +1,7 @@
 
 class BowlingGame:
     def __init__(self):
+        self.score = 0
+
+    def roll(self, pins_knocked_over):
+        self.score += pins_knocked_over
--- bowling_test.py
+++ bowling_test.py
@@ -3,6 +3,26 @@ from bowling import BowlingGame
 
 class BowlingGameTests(unittest.TestCase):
     def setUp(self):
+        self.bowling_game = BowlingGame()
+
+    def test_gutter_game(self):
+        #Arrange
+        expected_score = 0
+        
+        #Act
+        self.bowling_game.roll(0)
+        self.bowling_game.roll(0)
+        self.bowling_game.roll(0)
+        self.bowling_game.roll(0)
+        self.bowling_game.roll(0)
+        self.bowling_game.roll(0)
+        self.bowling_game.roll(0)
+        self.bowling_game.roll(0)
+        self.bowling_game.roll(0)
+        self.bowling_game.roll(0)
+        
+        #Assert
+        self.assertEqual(self.bowling_game.score, expected_score)
 
 if __name__ == '__main__':
     unittest.main()
